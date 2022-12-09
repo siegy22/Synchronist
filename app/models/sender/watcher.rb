1e3ee72e8408d29a071a9f7c42118b47b135b646
Yves Siegrist <Elektron1c97@gmail.com>
2022-12-09 19:14:47 +0100

    Fix/Improve watcher
    Disable watcher if paths are not configured

--- app/models/sender/watcher.rb
+++ app/models/sender/watcher.rb
@@ -1,6 +1,10 @@
 module Sender
   class Watcher
-    def self.start!(path, source_path, send_path)
+    def self.start!(mode, path, source_path, send_path)
+      if mode != "sender" || !path || !source_path || !send_path
+        return Rails.logger.info("Watcher exiting, some/all paths are not configured.")
+      end
+
       listener = Listen.to(File.dirname(path)) do |modified, added, removed|
         if File.file?(path)
           sync = Sync.create!

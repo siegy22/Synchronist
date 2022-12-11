ef64d997dbd1729d1316dcb3e9a22a93b6dbb331
Yves Siegrist <Elektron1c97@gmail.com>
2022-12-12 00:28:55 +0100

    Fix rsync when adding to storage

--- app/jobs/receiver/store_file_job.rb
+++ app/jobs/receiver/store_file_job.rb
@@ -7,7 +7,7 @@ module Receiver
       end
 
       ReceivedFile.create!(path: file_path, size: File.size(Config.get!(:receiver_receive_folder).join(file_path)))
-      `rsync -t --remove-source-files #{Config.get!(:receiver_receive_folder).join(file_path)} #{storage_folder}`
+      `rsync -Rt --remove-source-files #{File.join(Config.get!(:receiver_receive_folder), "/")}.#{File.join("/", file_path)} #{storage_folder}`
     end
   end
 end

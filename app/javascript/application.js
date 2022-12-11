39999f88fbe154dcf74f29a100d9472f41113d03
Yves Siegrist <Elektron1c97@gmail.com>
2022-12-11 21:28:54 +0100

    Disable bootstrap javascript

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,5 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 // import "startbootstrap-sb-admin-2/js/sb-admin-2.js"
 import "@hotwired/turbo-rails"
-import * as bootstrap from "bootstrap"
+// import * as bootstrap from "bootstrap"
 import "controllers"

2a016e4396f14b1ef8482a4fc090a99e9838aafd
Yves Siegrist <Elektron1c97@gmail.com>
2022-12-16 20:34:07 +0100

    Allow filtering of received files

--- app/controllers/received_files_controller.rb
+++ app/controllers/received_files_controller.rb
@@ -1,5 +1,8 @@
 class ReceivedFilesController < ApplicationController
   def index
     @received_files = ReceivedFile.ordered.limit(50)
+    if params.key?(:filename)
+      @received_files = @received_files.where("path ILIKE ?", "%#{params[:filename]}%")
+    end
   end
 end

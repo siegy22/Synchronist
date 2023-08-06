818d5c518ca6ff0e79381112d0f494380b235aad
Yves Siegrist <Elektron1c97@gmail.com>
2023-08-06 22:06:38 +0200

    Optimize send evne more
    - Calling File.size only once

--- app/models/sender/send.rb
+++ app/models/sender/send.rb
@@ -2,25 +2,25 @@ module Sender
   class Send
     def self.send(source_path, files, target_path, sync)
       Dir.chdir(source_path) do
-        files_count = files.count
-        sent_files = []
-        progress_step = (1.0 / files_count * 100.0)
+        progress_step = (1.0 / files.count * 100.0)
         progress = 0.0
-        finish = Proc.new do |file|
+        finish = Proc.new do |_file, _index, result|
           progress += progress_step
           sync.progress = progress.round
-          sync.increment(:bytes_transferred, File.size(file))
+          sync.increment(:bytes_transferred, result[:size])
           sync.save if sync.progress_changed?
         end
+
         sent_files = Parallel.map_with_index(files, finish: finish) do |file, index|
           `rsync -Rt #{file} #{target_path}`
           { path: file, size: File.size(file), sync_id: sync.id}
         end
+
         SentFile.insert_all(sent_files)
+        sync.increment(:progress, 100.0) if files.empty?
         sync.save
       end
 
-      sync.increment(:progress, COPYING_PROGRESS) if files.empty?
     end
   end
 end
